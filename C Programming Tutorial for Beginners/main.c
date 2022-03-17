4869a2a272382c114f32961f30de942ad9d6fe3c
willisman31 <58826505+willisman31@users.noreply.github.com>
2022-03-16 22:11:33 -0400

    a

---
+++ C Programming Tutorial for Beginners/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+# Drawing a shape
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");
+    printf("/___|\n");
+
+    return 0;
+}
